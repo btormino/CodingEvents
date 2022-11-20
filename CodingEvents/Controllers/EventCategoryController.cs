d42b4ab70dac2df98ce726eaf18b4346ef8cceb8
Bethany <morphforlife@gmail.com>
2022-11-19 22:15:41 -0600

    Finished Studio 16, also tried to get Edit working....did not succeed...yet

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -19,22 +19,23 @@ namespace CodingEvents.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.title = "All Categories";
+            //ViewBag.title = "All Categories";
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
         }
 
         [HttpGet]
-        [Route("EventCategory/Create")]
+        //[Route("EventCategory/Create")]
         public IActionResult Create()
         {
-            ViewBag.title = "Create";
+            //ViewBag.title = "Create";
             AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
 
             return View(addEventCategoryViewModel);
         }
 
         [HttpPost]
+        //[Route("EventCategory/Create")]
         public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
         {
             if (ModelState.IsValid)
@@ -49,10 +50,10 @@ namespace CodingEvents.Controllers
                 context.Categories.Add(newEventCategory);
                 context.SaveChanges();
 
-                return Redirect("EventCategory");
+                return Redirect("/EventCategory");
             }
 
-            return View(addEventCategoryViewModel);
+            return View("Create", addEventCategoryViewModel);
         }
 
     }
--- CodingEvents/ViewModels/AddEventCategoryViewModel.cs
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -5,7 +5,8 @@ using System.ComponentModel.DataAnnotations;
 namespace CodingEvents.ViewModels
 {
     public class AddEventCategoryViewModel
-    {       [Required(ErrorMessage = "Name is required.")]
+    {       
+        [Required(ErrorMessage = "Name is required.")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 20 characters.")]
         public string Name { get; set; }
     }
